78d5d99c4b6866961e3f0ef5be7045f6cb596a60
JoseManuel <josemcastilloc@gmail.com>
2021-06-17 22:23:33 -0300

    pre_admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Project
 
 # Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    readonly_fields = ('ceated', 'updated')
+
+#admin.site.register(Project,ProjectAdmin)
+
+admin.site.register(Project)
+
--- portfolio/models.py
+++ portfolio/models.py
@@ -3,8 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Project(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, verbose_name="Titulo")
     description = models.TextField()
     image = models.ImageField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "proyecto"
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.title
