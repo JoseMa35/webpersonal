89176da09d36bb2a8bfef74bd1da4b86e39f1416
JoseManuel <josemcastilloc@gmail.com>
2021-06-22 12:02:16 -0300

    WebPersonalatunear

--- portfolio/models.py
+++ portfolio/models.py
@@ -6,8 +6,10 @@ class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name="Titulo")
     description = models.TextField()
     image = models.ImageField(upload_to="projects")
+    link = models.URLField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
 
     class Meta:
         verbose_name = "proyecto"
